ad7232d4e2ceac7bf6aadfcd613b18125484ae13
dotcomnerd <Tomxxxfresh@gmail.com>
2025-02-06 10:25:00 +0000

    bump access token expiration

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET || "OMG A SUPER SECRET";
 
 export const generateToken = (payload: object): string => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
 };
 
 export const verifyToken = (token: string | undefined) => {
